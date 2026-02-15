d6cccccc7d485ce697951bcfb8cda6578f5e0324
siddh <sdawle04@gmail.com>
2026-02-15 12:25:45 +0530

    added calculator.sh

---
+++ calculator.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+
+##Author: Siddheshwar Dawle
+#Date: 15-Feb-2026
+#
+#
+echo "Enter the first number:"
+read a
+echo "Enter the second number:"
+read b
+
+
+echo "Addition: $(( a + b ))"
+echo "Substraction: $(( a - b ))"
+echo "Multiplication: $(( a * b ))"
+echo "Devidation: $(( a / b ))"
